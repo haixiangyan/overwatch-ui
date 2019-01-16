ae4e392907eacecde78de008b52132fa520d18fa
Haixiang <haixiang6123@gmail.com>
2019-01-16 11:52:16 -0800

    Add: github deployment config files

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:Haixiang6123/overwatch-ui.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/overwatch-ui/'
+        : '/',
     css: {
         loaderOptions: {
             sass: {
