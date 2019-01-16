fc65782cb7473b93f513f9575637ea3edbcbda8c
Haixiang <haixiang6123@gmail.com>
2019-01-16 14:59:25 -0800

    Add Karma config details

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,16 @@ module.exports = {
     },
     pluginOptions: {
         karma: {
+            // base path that will be used to resolve all patterns (eg. files, exclude)
+            basePath: '',
+            // frameworks to use
+            // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+            frameworks: ['mocha', 'sinon-chai'],
+            client: {
+                chai: {
+                    includeStack: true
+                }
+            },
             /**
              * Files containing tests. These will be preprocessed with webpack and
              * then loaded into karma.
@@ -23,7 +33,25 @@ module.exports = {
                 'tests/**/*.spec.js',
                 'tests/**/*.spec.ts',
             ],
-
+            // test results reporter to use
+            // possible values: 'dots', 'progress'
+            // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+            reporters: ['progress'],
+            // web server port
+            port: 9876,
+            // enable / disable colors in the output (reporters and logs)
+            colors: true,
+            // enable / disable watching file and executing tests whenever any file changes
+            autoWatch: true,
+            // start these browsers
+            // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+            browsers: ['ChromeHeadless'],
+            // Continuous Integration mode
+            // if true, Karma captures browsers, runs the tests and exits
+            singleRun: false,
+            // Concurrency level
+            // how many browser should be started simultaneous
+            concurrency: Infinity,
             /**
              * Takes an object with keys `port` (a Number) and `setup` (a function
              * taking an express App object). You can use this to set up an
