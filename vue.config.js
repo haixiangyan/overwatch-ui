8adf90f901e9662f9bcc0865b2388b556822323a
Haixiang <haixiang6123@gmail.com>
2019-01-16 14:54:11 -0800

    Config Karma with vue-cli-plugin-unit-karma successfully

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,33 @@ module.exports = {
                 `
             }
         }
+    },
+    pluginOptions: {
+        karma: {
+            /**
+             * Files containing tests. These will be preprocessed with webpack and
+             * then loaded into karma.
+             */
+            files: [
+                'tests/**/*.spec.js',
+                'tests/**/*.spec.ts',
+            ],
+
+            /**
+             * Takes an object with keys `port` (a Number) and `setup` (a function
+             * taking an express App object). You can use this to set up an
+             * Express server that will run alongside your tests.
+             */
+            expressServer: undefined,
+
+            /**
+             * If provided, this will be merged into the base karma config for
+             * the plugin.
+             *
+             * See: https://github.com/webpack-contrib/karma-webpack and
+             * http://karma-runner.github.io/2.0/config/configuration-file.html
+             */
+            karmaConfig: undefined
+        }
     }
 }
