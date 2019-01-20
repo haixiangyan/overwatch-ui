6db605114d41888ef8994a4e3587c4d0912c88df
Haixiang <haixiang6123@gmail.com>
2019-01-20 14:02:27 -0800

    Fix: remove duplicate generated OwAlert

--- src/plugins/owAlert.js
+++ src/plugins/owAlert.js
@@ -1,14 +1,25 @@
 import OwAlert from '../components/OwAlert'
+
+let currentOwAlert
+
 export default {
     install(Vue, options) {
         // Add function $owToast to prototype
         Vue.prototype.$owAlert = function (owAlertOptions) {
-            let OwAlertExt = Vue.extend(OwAlert)
-            let owAlert = new OwAlertExt({
-                propsData: owAlertOptions
-            })
-            owAlert.$mount()
-            document.body.appendChild(owAlert.$el)
+            if (currentOwAlert) {
+                currentOwAlert.close()
+            }
+            currentOwAlert = createOwAlert(Vue, owAlertOptions)
         }
     }
+}
+
+// Create OwAlert
+function createOwAlert(Vue, owAlertOptions) {
+    let OwAlertExt = Vue.extend(OwAlert)
+    let owAlert = new OwAlertExt({ propsData: owAlertOptions })
+    owAlert.$mount()
+    document.body.appendChild(owAlert.$el)
+
+    return owAlert
 }
