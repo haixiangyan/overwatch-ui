f4a3f39d3f47fea5b58ebbd53592d9c8f513c8fd
Haixiang <haixiang6123@gmail.com>
2019-01-19 22:08:36 -0800

    Add OwSider test cases

---
+++ tests/unit/OwSider.spec.js
@@ -0,0 +1,35 @@
+import {expect} from 'chai'
+import {shallowMount} from '@vue/test-utils'
+import OwSider from '../../src/components/OwSider.vue'
+
+const testData = {
+    slot: 'Hello',
+    visible: false
+}
+
+describe('OwSider.vue', () => {
+    it('should exist', () => {
+        const OwSiderWrapper = shallowMount(OwSider)
+        expect(OwSiderWrapper).to.exist
+    })
+
+    it('should pass elements to slot', () => {
+        const {slot} = testData
+        const OwSiderWrapper = shallowMount(OwSider, {
+            slots: {
+                default: slot
+            }
+        })
+
+        expect(OwSiderWrapper.text()).to.equal(slot)
+    })
+    it('should pass props.visible to slot', () => {
+        const {slot, visible} = testData
+        const OwSiderWrapper = shallowMount(OwSider, {
+            propsData: {visible},
+            slot: {default: slot}
+        })
+
+        expect(OwSiderWrapper.text()).to.equal('')
+    })
+})
