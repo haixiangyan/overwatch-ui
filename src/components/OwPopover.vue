705bf2224842b7b11155a5d3f2e68f4a5b381156
Haixiang <haixiang6123@gmail.com>
2019-01-21 15:33:43 -0800

    Add hide function to OwPopover

--- src/components/OwPopover.vue
+++ src/components/OwPopover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="ow-popover" @click="onClick">
-        <div v-if="visible" class="ow-popover-content-wrapper">
+    <div class="ow-popover" @click.stop="onClick">
+        <div v-if="visible" class="ow-popover-content-wrapper" @click.stop>
             <slot name="content"></slot>
         </div>
         <!--Trigger-->
@@ -19,22 +19,38 @@
         methods: {
             onClick() {
                 this.visible = !this.visible
+                if (this.visible) {
+                    // Avoid clicking twice
+                    this.$nextTick(() => {
+                        console.log('add')
+                        let eventHandler = () => {
+                            this.visible = false
+                            console.log('document hide')
+                            // Remove event listener after closing OwPopover
+                            document.removeEventListener('click', eventHandler)
+                        }
+                        document.addEventListener('click', eventHandler)
+                    })
+                }
+                else {
+                    console.log('vm hide')
+                }
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-.ow-popover {
-    position: relative;
-    display: inline-block;
-    vertical-align: top;
+    .ow-popover {
+        position: relative;
+        display: inline-block;
+        vertical-align: top;
 
-    &-content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
+        &-content-wrapper {
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            border: 1px solid red;
+        }
     }
-}
 </style>
