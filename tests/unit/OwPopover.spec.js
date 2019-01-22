5afd3ea224fa76e96267bb75b95b3737af315482
Haixiang <haixiang6123@gmail.com>
2019-01-21 21:21:21 -0800

    Add OwPopover test cases

---
+++ tests/unit/OwPopover.spec.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai'
+import {shallowMount} from '@vue/test-utils'
+import OwPopover from '../../src/components/OwPopover.vue'
+
+const testData = {
+    position: 'right',
+    triggers: ['click', 'hover'],
+    contentSlot: `<div id="hello">Popoverjklajdklfjaklsfjlkasjfkljaslkfjlksjfljsdlfjlskjdf</div>`,
+    triggerSlot: '<button>Top</button>'
+}
+
+describe('OwPopover.vue', () => {
+    it('exists', () => {
+        const OwPopoverWrapper = shallowMount(OwPopover)
+        expect(OwPopoverWrapper).to.exist
+    })
+})
