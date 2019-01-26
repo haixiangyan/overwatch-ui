8a167c481ffcb7337c1e9083d53211f421fb6803
Haixiang6123 <haixiang6123@gmail.com>
2019-01-26 14:17:35 -0800

    Add codee split

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,17 @@ module.exports = {
             }
         }
     },
+    configureWebpack: {
+        performance: {
+            hints: false
+        },
+        optimization: {
+            splitChunks: {
+                minSize: 10000,
+                maxSize: 250000,
+            }
+        }
+    },
     pluginOptions: {
         karma: {
             /**
