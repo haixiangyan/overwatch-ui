ae2b655e25513af92f11659bd449c5bccf6f94c9
Haixiang <haixiang6123@gmail.com>
2019-01-30 22:28:52 -0800

    Add OwCascader component

---
+++ src/components/OwCascader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ow-cascader">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OwCascader"
+    }
+</script>
+
+<style scoped lang="scss">
+.ow-cascader {
+
+}
+</style>
