e86eaab89032163c963619f6fb28c33fd1576957
Haixiang <haixiang6123@gmail.com>
2019-02-23 12:56:48 -0800

    Add scroll function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,112 @@
 <template>
     <div id="app">
-        <ow-date-picker :value="date" @input="date = $event">
-        </ow-date-picker>
-        <p>Hello</p>
+        <!--<ow-date-picker :value="date" @input="date = $event">-->
+        <!--</ow-date-picker>-->
+        <!--<p>Hello</p>-->
+        <div class="parent" ref="parent">
+            <div ref="child" class="child">
+                <p>text1</p>
+                <p>text2</p>
+                <p>text3</p>
+                <p>text4</p>
+                <p>text5</p>
+                <p>text6</p>
+                <p>text7</p>
+                <p>text8</p>
+                <p>text9</p>
+                <p>text10</p>
+                <p>text11</p>
+                <p>text12</p>
+                <p>text13</p>
+                <p>text14</p>
+                <p>text15</p>
+                <p>text16</p>
+                <p>text17</p>
+                <p>text18</p>
+                <p>text19</p>
+                <p>text20</p>
+                <p>text21</p>
+                <p>text22</p>
+                <p>text23</p>
+                <p>text24</p>
+                <p>text25</p>
+                <p>text26</p>
+                <p>text27</p>
+                <p>text28</p>
+                <p>text29</p>
+                <p>text30</p>
+                <p>text31</p>
+                <p>text32</p>
+                <p>text33</p>
+                <p>text34</p>
+                <p>text35</p>
+                <p>text36</p>
+                <p>text37</p>
+                <p>text38</p>
+                <p>text39</p>
+                <p>text40</p>
+                <p>text41</p>
+                <p>text42</p>
+                <p>text43</p>
+                <p>text44</p>
+                <p>text45</p>
+                <p>text46</p>
+                <p>text47</p>
+                <p>text48</p>
+                <p>text49</p>
+                <p>text50</p>
+                <p>text51</p>
+                <p>text52</p>
+                <p>text53</p>
+                <p>text54</p>
+                <p>text55</p>
+                <p>text56</p>
+                <p>text57</p>
+                <p>text58</p>
+                <p>text59</p>
+                <p>text60</p>
+                <p>text61</p>
+                <p>text62</p>
+                <p>text63</p>
+                <p>text64</p>
+                <p>text65</p>
+                <p>text66</p>
+                <p>text67</p>
+                <p>text68</p>
+                <p>text69</p>
+                <p>text70</p>
+                <p>text71</p>
+                <p>text72</p>
+                <p>text73</p>
+                <p>text74</p>
+                <p>text75</p>
+                <p>text76</p>
+                <p>text77</p>
+                <p>text78</p>
+                <p>text79</p>
+                <p>text80</p>
+                <p>text81</p>
+                <p>text82</p>
+                <p>text83</p>
+                <p>text84</p>
+                <p>text85</p>
+                <p>text86</p>
+                <p>text87</p>
+                <p>text88</p>
+                <p>text89</p>
+                <p>text90</p>
+                <p>text91</p>
+                <p>text92</p>
+                <p>text93</p>
+                <p>text94</p>
+                <p>text95</p>
+                <p>text96</p>
+                <p>text97</p>
+                <p>text98</p>
+                <p>text99</p>
+                <p>text100</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,6 +118,46 @@
                  date: new Date()
             }
         },
+        mounted() {
+            console.log(navigator.userAgent)
+            function winScroll() {
+                if (event.deltaY > 0) {
+                    console.log('想看上面')
+                }
+                else if (event.deltaY < 0) {
+                    console.log('想看下面')
+                }
+                else {
+                    console.log('没动')
+                }
+            }
+            let translateY = 0
+            function macScroll() {
+                if (event.deltaY > 0) {
+                    translateY -= 10
+                    child.style.transform = `translateY(${translateY}px)`
+                }
+                else if (event.deltaY < 0) {
+                    translateY += 10
+                    child.style.transform = `translateY(${translateY}px)`
+                }
+                else {
+                    console.log('没动')
+                }
+            }
+            const parent = this.$refs.parent
+            const child = this.$refs.child
+            child.style.transition= 'transform .5s linear'
+            if (navigator.userAgent.indexOf('Mac OS X')) {
+                parent.addEventListener('wheel', macScroll)
+            }
+            else {
+                parent.addEventListener('wheel', winScroll)
+            }
+            parent.addEventListener('touchmove', () => {
+                console.log('touch')
+            })
+        }
     }
 </script>
 
@@ -22,5 +166,14 @@
         height: 100vh;
         padding: 100px 100px;
         background: url("./assets/images/bg.png");
+        .parent {
+            border: 4px solid red;
+            height: 400px;
+            overflow: hidden;
+            box-sizing: border-box;
+            .child {
+                border: 4px solid green;
+            }
+        }
     }
 </style>
