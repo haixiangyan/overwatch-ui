773dcb81633e17c416f731233c92fbb1312c1ff4
Haixiang6123 <haixiang6123@gmail.com>
2019-02-25 22:09:06 -0800

    Update Popover test cases

--- tests/unit/Popover/OwPopover.spec.js
+++ tests/unit/Popover/OwPopover.spec.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai'
-import {shallowMount} from '@vue/test-utils'
+import {shallowMount, mount} from '@vue/test-utils'
 import OwPopover from '../../../src/components/Popover/OwPopover.vue'
+import OwButton from '../../../src/components/Button/OwButton'
 
 const testData = {
     position: 'right',
@@ -13,5 +14,52 @@ describe('OwPopover.vue', () => {
     it('exists', () => {
         const OwPopoverWrapper = shallowMount(OwPopover)
         expect(OwPopoverWrapper).to.exist
+    })
+    it('receives position', () => {
+        const OwPopoverWrapper = mount(OwPopover, {
+            propsData: {
+                position: 'left'
+            },
+            slots: {
+                default: `<ow-button>Pop It!</ow-button>`,
+                content: `<div>Popover Content</div>`
+            },
+            stubs: {OwButton},
+            attachToDocument: true
+        })
+
+        OwPopoverWrapper.find(OwButton).trigger('click')
+        expect(OwPopoverWrapper.find('.ow-popover-content-wrapper-left').exists()).to.equal(true)
+    })
+    it('receives trigger', () => {
+        const OwPopoverWrapper = mount(OwPopover, {
+            propsData: {
+                trigger: 'hover'
+            },
+            slots: {
+                default: `<ow-button>Pop It!</ow-button>`,
+                content: `<div>Popover Content</div>`
+            },
+            stubs: {OwButton},
+            attachToDocument: true
+        })
+        OwPopoverWrapper.trigger('mouseenter')
+        expect(OwPopoverWrapper.find('.ow-popover-content-wrapper').exists()).to.equal(true)
+    })
+    it('can unmount listeners when destroy', () => {
+        const OwPopoverWrapper = mount(OwPopover, {
+            propsData: {
+                position: 'left'
+            },
+            slots: {
+                default: `<ow-button>Pop It!</ow-button>`,
+                content: `<div>Popover Content</div>`
+            },
+            stubs: {OwButton},
+            attachToDocument: true
+        })
+
+        OwPopoverWrapper.destroy()
+        expect(OwPopoverWrapper.element.onclick).to.equal(null)
     })
 })
